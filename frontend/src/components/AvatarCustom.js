ac64d87f1cd098d876d6b849f1fbbc9f1e109187
Zayooo00 <konrad6263@o2.pl>
2022-04-05 17:23:03 +0200

    (#2) Layout changes

--- frontend/src/components/AvatarCustom.js
+++ frontend/src/components/AvatarCustom.js
@@ -5,7 +5,7 @@ function AvatarCustom({ name }) {
     return (
         <Box display="flex" justifyContent="center" alignItems="center" flexDirection= "column">
             <Avatar>{name[0]}</Avatar>
-            <Typography variant="small"  type="body2" color="text.secondary" style={{color: 'black', width: '80px', whiteSpace: 'nowrap',
+            <Typography variant="subtitle3"  type="body2" color="text.secondary" style={{color: 'black', width: '80px', whiteSpace: 'nowrap',
              overflow: 'hidden', textOverflow: 'ellipsis', marginLeft: '20px', marginRight: '20px'}}>{name}</Typography>
         </Box>
     );
--- frontend/src/components/Timeline.js
+++ frontend/src/components/Timeline.js
@@ -5,7 +5,7 @@ function Timeline({ name }) {
     return (
         <Box display="flex" justifyContent="center" alignItems="center" flexDirection= "center">
             <Avatar>{name[0]}</Avatar>
-            <Typography variant="small"  type="body2" color="text.secondary" style={{color: 'black', textOverflow: 'ellipsis', marginLeft: '10px', marginRight: '0px'}}>{name}</Typography>
+            <Typography type="body2" color="text.secondary" style={{color: 'black', textOverflow: 'ellipsis', marginLeft: '10px', marginRight: '0px'}}>{name}</Typography>
         </Box>
     );
 }
